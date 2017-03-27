f1c69b9714e1b31108c45a0ad95b30ac542e4001
Takara Small <a.simpsonlately@gmail.com>
2017-03-27 11:44:19 -0400

    Added more to about profile

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -12,4 +12,30 @@ $(function(){      console.log('yo, this is a test');
       }
     }
   });
+  
+  // function toggle_color(color1, color2, cycle_time, wait_time) {
+
+  //   setInterval(function first_color() {
+  //       document.body.style.backgroundColor = color1;
+  //       setTimeout(change_color, wait_time);
+  //   }, cycle_time);
+
+  //   function change_color() {
+  //       document.body.style.backgroundColor = color2;
+  //   }
+
+
+// (function() {
+//     var s = document.getElementById('titleTable').style,
+//         f = false,
+//         c1 = '#000000',
+//         c2 = '#ffffff';
+
+//     setInterval(function() {
+//         s.backgroundColor = f ? c1 : c2;
+//         s.color = f ? c2 : c1;
+//         f = !f;
+//     }, 500);
+// })();
+
  }); 
